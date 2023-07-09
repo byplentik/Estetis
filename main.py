b1d78b2186d3b0a71541c4db46876014e4bbb52c
plentik <bplentik@gmail.com>
2023-07-09 12:09:38 +0300

    добавил метод update_task и также добавил описание к документации

--- main.py
+++ main.py
@@ -10,12 +10,26 @@ from models import Task
 
 load_dotenv('.env')
 
-app = FastAPI()
+description = """
+    Это api выполненное в рамках тестового задания. 
+    Есть 3 конечные точки
+    - /tasks/add/ `POST` Для создания задачи
+    - /tasks/ `GET` Для отображения всех существующих задач
+    - /tasks/{uuid} `PUT` Для обновления задачи по уникальному идентификатору
+    """
+
+tags_metadata = [{'name': 'Tasks',}]
+
+app = FastAPI(
+    title='Estetis api - тестовое задание',
+    description=description
+)
 
 app.add_middleware(DBSessionMiddleware, db_url=os.environ['DATABASE_URL'])
 
 
-@app.post('/tasks/add', status_code=status.HTTP_201_CREATED)
+@app.post('/tasks/add', status_code=status.HTTP_201_CREATED,
+          description='Endpoint который добавляет задачу в БД', tags=['Tasks'])
 async def create_task(task: TaskIn):
     try:
         task_obj = Task(description=task.description, params=task.params)
@@ -27,8 +41,27 @@ async def create_task(task: TaskIn):
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(ex))
 
 
-@app.get('/tasks', response_model=list[TaskOut])
+@app.get('/tasks', response_model=list[TaskOut], description='Endpoint который получает все существующие задачи',
+         tags=['Tasks'])
 async def get_all_tasks():
     tasks = db.session.query(Task).all()
     return [TaskOut(task_uuid=str(task.task_uuid), description=task.description, params=task.params) for task in tasks]
 
+
+@app.put('/tasks/{uuid}', description='Обновляет задачу с заданным идентификатором с видом UUID',
+         tags=['Tasks'])
+async def update_task(uuid: str, task: TaskIn):
+    task_obj = db.session.query(Task).filter(Task.task_uuid == uuid).first()
+
+    if not task_obj:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Task not found')
+
+    task_obj.description = task.description
+    task_obj.params = task.params
+
+    try:
+        db.session.commit()
+        return {"message": "Task updated successfully"}
+    except Exception as e:
+        db.session.rollback()
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
--- schemas.py
+++ schemas.py
@@ -1,12 +1,35 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, constr
 
 
 class TaskIn(BaseModel):
     description: str = Field(..., title="Описание задачи")
     params: dict = Field(..., title="Параметры задачи")
 
+    class Config:
+        schema_extra = {
+            "example": {
+                "description": "Описание задачи",
+                "params": {
+                    "param_1": "Параметр 1",
+                    "param_2": 2
+                }
+            }
+        }
+
 
 class TaskOut(BaseModel):
     task_uuid: str = Field(..., title="UUID задачи")
     description: str = Field(..., title="Описание задачи")
     params: dict = Field(..., title="Параметры задачи")
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "task_uuid": "Уникальный идентификатор задачи в формате UUID",
+                "description": "Описание задачи",
+                "params": {
+                    "param_1": "Параметр 1",
+                    "param_2": 2
+                }
+            }
+        }
